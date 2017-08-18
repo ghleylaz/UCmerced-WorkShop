5fd7f9e2b760b2ccfa1fec57f84b069199ff14b6
ghazaaleleylaz <ghazaaleleylaz@Ghazaales-MacBook-Pro.local>
2017-08-18 10:02:29 -0700

    add new data

--- 2017-08-17-my-script.R
+++ 2017-08-17-my-script.R
@@ -5,6 +5,8 @@
 # download.file("https://ndownloader.figshare.com/files/2292169",     
 #               "data/portal_data_joined.csv")
 
+## hello
+
 ## read in data into R ----
 surveys <- read.csv("data/portal_data_joined.csv")
 
